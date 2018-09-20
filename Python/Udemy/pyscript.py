3acc60b12be99a58470de8a1b187c79a1347cbff
crazyfables <43083257+crazyfables@users.noreply.github.com>
2018-09-20 12:46:42 -0500

    Testing commit through Pycharm.

---
+++ Python/Udemy/pyscript.py
@@ -0,0 +1,9 @@
+# Just a simple tutorial
+
+print("\nHello World")
+
+# TODO: Fix breaks
+
+# TODO: Write more code
+
+# TODO: Never give up, never surrender!
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Academics
+
+# Nope, you got me, that's all I have. 
