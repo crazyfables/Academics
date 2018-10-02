5421507861d828e7b8e3a76697532501b3b26b04
crazyfables <43083257+crazyfables@users.noreply.github.com>
2018-10-02 14:59:48 -0500

    Created Guess My Number and Calculator scripts.

---
+++ Python/calculator.py
@@ -0,0 +1,59 @@
+# By Jessica Angela Campisi
+# Calculator
+# 10/2/2018
+# Description: A simple calculator. Using Functions, If/Else, int conversion
+
+# define functions
+# addition
+
+
+def add(a: int, b: int):
+    return a + b
+
+
+# subtraction
+def subtract(a: int, b: int):
+    return a - b
+
+
+# multiply
+def multiply(a: int, b: int):
+    return a * b
+
+
+# divide
+def divide(a: int, b: int):
+    return a / b
+
+
+# modulus
+def mod(a: int, b: int):
+    return a % b
+
+# instructions
+def instructions():
+    print("Do you want to... "
+          "\n1....Add"
+          "\n2....Subtraction"
+          "\n3....Multiply"
+          "\n4....Divide"
+          "\n5....Modulus")
+
+instructions()
+choice = int(input("Enter choice: "))
+
+num1 = int(input("What is the first number: "))
+num2 = int(input("What is the second number: "))
+
+if choice == 1:
+    print("\nTotal: {}".format(add(num1, num2)))
+elif choice == 2:
+    print("\nTotal: {}".format(subtract(num1, num2)))
+elif choice == 3:
+    print("\nTotal: {}".format(multiply(num1, num2)))
+elif choice == 4:
+    print("\nTotal: {}".format(divide(num1, num2)))
+elif choice == 5:
+    print("\nRemainder: {}".format(mod(num1, num2)))
+else:
+    print("\nIncorrect entry")
---
+++ Python/guess.py
@@ -0,0 +1,41 @@
+# By Jessica Angela Campisi
+# Guess My Number
+# 10/1/2018
+# Description: The player will guess a number between 1 and 100. The computer will let them know if they are high or
+# low.
+
+# Imports
+import random, sys
+
+# Variables
+random.seed()
+randnum = random.randrange(1, 100)
+playerGuess = 0
+lastGuess = 0
+playerWin = False
+
+# Testing Random
+print("Random number: {}".format(randnum))
+
+# Introduction
+print("Welcome to Guess My Number!. "
+      "\nPlease enter a number between 1 and 100."
+      "\nWhen you guess closer the computer will tell you warmer."
+      "\nWhen you guess farther the computer will tell you closer."
+      "\nGuess the number to win!")
+
+# Game Loop
+while playerWin is False:
+    playerGuess = int(input("Please enter a guess: "))
+
+    if playerGuess is randnum:
+        print("\nYou Win!")
+        playerWin = True
+    elif playerGuess > randnum:
+        print("\nGuess is too High!")
+    elif playerGuess < randnum:
+        print("\nGuess is too low!")
+
+
+
+# End of Game Loop
