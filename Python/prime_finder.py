fb8a71f0c5a82db13413fcc9b680753f4a4e8b7a
crazyfables <43083257+crazyfables@users.noreply.github.com>
2018-11-12 11:35:27 -0600

    Added some comments to prime_finder.py

--- Python/prime_finder.py
+++ Python/prime_finder.py
@@ -11,23 +11,27 @@ class Prime:
     min = 2
     max = 0
 
+    # returns true if num has no divisions except itself and 2.
     def is_prime(self, num):
         for x in range(2, num-1):
             if num % x is 0:
                 return False
         return True
 
+    # runs the program so that nothing runs globally.
     def run(self):
         print("Enter a number. This program will return every prime number between 1 and that number.")
         self.number_request()
         self.print_prime()
 
+    # prints if is_prime() returns true.
     def print_prime(self):
 
         for x in range(self.min, self.max+1):
             if self.is_prime(x):
                 print(x)
 
+    # get the big number from the end user
     def number_request(self):
         self.max = int(input("Please enter a large number: "))
 
